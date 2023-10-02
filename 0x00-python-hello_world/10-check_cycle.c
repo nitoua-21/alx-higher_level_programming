bb43b283de5667429ca49df3938d79b2e1851afa
Normich ITOUA <nitoua.dev@gmail.com>
2023-10-02 09:27:15 -0700

    Add Task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - hecks if a singly linked list has a cycle in it.
+ * @list: pointer to linked list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list, *fast = list;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
