e9fb9b4b0adec5723d6ecf2b221bacc949a9ef80
Normich <nitoua.dev@gmail.com>
2023-10-08 02:42:23 -0700

    Add task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """Get an element from the list
+
+    Args:
+        my_list: list
+        idx: index of element to retrieve
+
+    Returns:
+        Value of eleement at index idx
+    """
+
+    return my_list[idx]
