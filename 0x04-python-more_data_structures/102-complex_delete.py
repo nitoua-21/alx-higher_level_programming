09f7cf8bade1c5ab3e7058a9970bf972c52531d3
Normich <nitoua.dev@gmail.com>
2023-10-11 13:33:09 -0700

    Fix Shebang

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,4 @@
-#!/usr/python3
+#!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     keys_to_del = [key for key, val in a_dictionary.items() if val == value]
     for key in keys_to_del:
