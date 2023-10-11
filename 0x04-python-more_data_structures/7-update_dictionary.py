d1dc56cea4a3f0741a2387758640e06318687c32
Normich <nitoua.dev@gmail.com>
2023-10-11 11:42:55 -0700

    Add Task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    d = a_dictionary.copy()
-    d[key] = value
-    return d
+    a_dictionary[key] = value
+    return a_dictionary
