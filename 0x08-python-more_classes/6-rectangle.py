08d6ee3d5be1f08874783ad44217be27b17b5d8a
Normich <nitoua.dev@gmail.com>
2023-11-05 03:20:46 -0800

    Update Task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle():
         """
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -82,5 +82,5 @@ class Rectangle():
 
     def __del__(self):
         """ Deletes instance of class """
-        Rectangle.number_of_instance -= 1
+        type(self).number_of_instance -= 1
         print("Bye rectangle...")
