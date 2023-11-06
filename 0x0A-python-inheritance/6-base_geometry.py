5b2bd9b03cbeceede2b9a877ec9a1e6b2d857f28
Normich <nitoua.dev@gmail.com>
2023-11-06 13:51:31 -0800

    Add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+
+Contains class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """Base class for geometry"""
+
+    def area(self):
+        """raises an Exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
