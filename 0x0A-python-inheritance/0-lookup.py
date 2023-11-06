6ef5f45768766bb9a0960a2244ff3c1497874ecb
Normich <nitoua.dev@gmail.com>
2023-11-06 09:49:44 -0800

    Add Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,18 @@
+#!/bin/usr/python3
+"""
+Module 0-lookup
+Contains function list
+"""
+
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object
+
+    Args:
+        obj (object): object
+
+    Returns:
+        List object
+    """
+    return dir(obj)
