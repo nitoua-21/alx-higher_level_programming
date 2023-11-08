c334dd8e76c5699d9d84f09e5f8e312b3a8aedbd
Normich <nitoua.dev@gmail.com>
2023-11-07 16:52:47 -0800

    Fix Task 101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -14,75 +14,58 @@ format: File size: <total size>
 format: <status code (in ascending order)>: <number>
 """
 
-import sys
 
-
-def print_size_and_codes(size, stat_codes):
+def print_metrics(total_size, status_codes):
     """
     Print the total file size and status code statistics.
 
     Args:
-        size (int): Total file size.
-        stat_codes (dict): A dictionary containing status
-        codes and their counts.
+        total_size (int): Total file size.
+        status_codes (dict): A dictionary containing status codes
+        and their counts.
 
     Prints:
         - File size: <total size>
-        - <status code (in ascending order)>: <number>
+        - <status code>: <number>
     """
-    print(f"File size: {size}")
-    for code, count in sorted(stat_codes.items()):
-        if count:
+    print(f"File size: {total_size}")
+    for code in sorted(status_codes):
+        count = status_codes.get(code, 0)
+        if count > 0:
             print(f"{code}: {count}")
 
 
-def parse_line(line, size, stat_codes):
-    """
-    Parse a single line and update the size
-    and status code statistics.
-
-    Args:
-        line (str): The input log line to parse.
-        size (int): Current total file size.
-        stat_codes (dict): A dictionary containing status codes
-        and their counts.
-
-    Returns:
-        int: Updated total file size.
-        dict: Updated status code statistics.
-    """
-    fields = line.strip().split()
-    size += int(fields[-1])
-    if fields[-2] in stat_codes:
-        stat_codes[fields[-2]] += 1
-    return size, stat_codes
-
-
-def parse_stdin_and_compute():
+def main():
     """
     Read lines from stdin, parse them, and compute statistics.
 
-    This function reads lines from stdin, parses them
-    using the `parse_line` function, and keeps track of total file size
-    and status code statistics. It prints these statistics every
-    10 lines and handles KeyboardInterrupt to provide results
-    in case of an interruption.
+    This function reads lines from stdin, parses them, keeps track
+    of total file size, and counts status code occurrences.
+    It prints these statistics every 10 lines and handles KeyboardInterrupt
+    to provide results in case of an interruption.
     """
-    size = 0
-    stat_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-                  "404": 0, "405": 0, "500": 0}
+    total_size = 0
+    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+                    "404": 0, "405": 0, "500": 0}
     lines = 0
 
     try:
         for line in sys.stdin:
-            size, stat_codes = parse_line(line, size, stat_codes)
-            lines += 1
-            if lines % 10 == 0:
-                print_size_and_codes(size, stat_codes)
+            fields = line.strip().split()
+            if len(fields) >= 9:
+                status_code = fields[-2]
+                size = int(fields[-1])
+                total_size += size
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+                lines += 1
+
+                if lines % 10 == 0:
+                    print_metrics(total_size, status_codes)
+
     except KeyboardInterrupt:
-        print_size_and_codes(size, stat_codes)
-        raise
+        print_metrics(total_size, status_codes)
 
 
 if __name__ == "__main__":
-    parse_stdin_and_compute()
+    main()
