ddb16ff121afe0149cb2cf81c49834b4e8776224
Normich ITOUA <nitoua.dev@gmail.com>
2024-04-16 22:28:55 +0100

    Add task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -11,6 +11,10 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Retrieves a dictionary representation of a Student instance"""
-        return self.__dict__
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: getattr(self, key) for key in attrs
+                    if hasattr(self, key)}
