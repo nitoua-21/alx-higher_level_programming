cca8ee8106dd7668d1ecc1efe91f84191697696d
Normich ITOUA <nitoua.dev@gmail.com>
2024-04-20 15:06:25 +0100

    Add task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/sur/bin/python3
+"""Module contains function is_kind_of_class(obj, a_class)"""
+
+
+def is_kind_of_class(obj, a_class):
+    """if the object is an instance of, or if the object is an instance of
+        a class that inherited from, the specified class
+    """
+    return isinstance(obj, a_class)
