6f0e4521c9f9791efa7fff8ed54aa3f9a45c4fa0
Normich ITOUA <nitoua.dev@gmail.com>
2024-04-20 15:45:55 +0100

    Add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Contains class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Basic geometry class"""
+    def area(self):
+        """Raises an Exception"""
+        raise Exception("area() is not implemented")
