98d8b20f6d9d6d2b5cac3275b05fe379a7c3d718
Normich ITOUA <nitoua.dev@gmail.com>
2024-04-20 17:28:14 +0100

    Add task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -12,9 +12,9 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-    
+
     def area(self):
         return self.__height * self.__width
-    
+
     def __str__(self):
         return F"[Rectangle] {self.__width}/{self.__height}"
