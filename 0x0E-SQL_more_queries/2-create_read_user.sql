3cb5ce34d90cfe44109c7d70623ad2e73b24a8a6
Normich ITOUA <nitoua.dev@gmail.com>
2024-05-22 23:56:39 +0100

    Add task 8

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,13 +2,7 @@
 -- user_0d_2 password should be set to user_0d_2_pwd
 -- should not fail if either already exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
 CREATE USER IF NOT EXISTS 'user_0d_2' @'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
 GRANT USAGE ON *.* TO 'user_0d_2' @'localhost';
-
-GRANT
-SELECT
-    ON `hbtn_0d_2`.* TO 'user_0d_2' @'localhost';
-
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2' @'localhost';
 FLUSH PRIVILEGES;
