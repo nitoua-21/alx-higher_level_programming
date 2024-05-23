2709f0cb032809c335d622af434f8cc7e445cb59
Normich ITOUA <nitoua.dev@gmail.com>
2024-05-23 01:35:28 +0100

    Update task 102

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
---  script that lists all shows from hbtn_0d_tvshows_rate by their rating.
--- ach record should display: tv_shows.title - rating sum
+-- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_shows.title - rating sum
 --Results must be sorted in descending order by the rating
 SELECT
     tv_shows.title,
@@ -8,7 +8,6 @@ FROM
     tv_shows
     JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY
-    1
+    title
 ORDER BY
     2 DESC;
-    
