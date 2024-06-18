62512a59029bff87e56f4525c7c2604ccdeee737
Normich ITOUA <nitoua.dev@gmail.com>
2024-06-18 07:19:01 -0700

    Add tsk 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const size = Number.parseInt(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let square = '';
+    for (let j = 0; j < size; j++) {
+      square += 'X';
+    }
+    console.log(square);
+  }
+}
