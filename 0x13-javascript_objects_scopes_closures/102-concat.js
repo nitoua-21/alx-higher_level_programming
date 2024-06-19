cd1f255590354c2a05ddc5f928d787c7f2a9c58b
Normich ITOUA <nitoua.dev@gmail.com>
2024-06-19 10:30:50 -0700

    Add task 13

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+const [,, fileA, fileB, fileC] = process.argv;
+
+fs.readFile(fileA, 'utf8', (err, dataA) => {
+  if (err) throw err;
+  fs.readFile(fileB, 'utf8', (err, dataB) => {
+    if (err) throw err;
+    fs.writeFile(fileC, dataA + dataB, (err) => {
+      if (err) throw err;
+    });
+  });
+});
