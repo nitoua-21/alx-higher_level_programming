69676671272fd9852da1dddc6419cb00cc857fb1
Normich ITOUA <nitoua.dev@gmail.com>
2024-06-22 12:13:00 +0100

    Update task 4

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                            db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute(
-        f"SELECT * FROM states WHERE name = '{sys.argv[4]}' ORDER BY id ASC")
+        f"SELECT * FROM states WHERE name = '{sys.argv[4]:s}' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
