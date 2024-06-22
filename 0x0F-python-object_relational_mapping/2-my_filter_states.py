d05067bc516b37250d2469951187ad6461e95a6f
Normich ITOUA <nitoua.dev@gmail.com>
2024-06-22 12:14:19 +0100

    Fix task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
         f"SELECT * FROM states WHERE name = '{sys.argv[4]:s}' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1] == sys.argv[4]:
+            print(row)
     cur.close()
     conn.close()
