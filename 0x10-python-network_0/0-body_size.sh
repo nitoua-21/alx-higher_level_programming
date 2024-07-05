3560322b483622fdf6ec1428c99e397a0a3f6168
Normich ITOUA <nitoua.dev@gmail.com>
2024-07-05 01:49:17 +0100

    Update task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/bash
-# This script takes a URL as an argument, sends a request to that URL using cURL,
-# and displays the size of the response body in bytes.
+#!/bin/bash
+# Sends a request to that URL using cURL, and displays the size of the response body in bytes.
 curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
