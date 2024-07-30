d8c1587ee9daf22635a26c1082f2ec69da22afe6
Normich ITOUA <nitoua.dev@gmail.com>
2024-07-31 00:46:52 +0100

    Add task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,12 @@
 #!/usr/bin/node
-
-let url = process.argv[2];
 const request = require('request');
-
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    let films = JSON.parse(body).results;
-    let count = 0;
-    for (let i in films) {
-      let chars = films[i].characters;
-      for (let c in chars) {
-	if (chars[c].includes('18')) {
-	  count++;
-	}
-      }
-    }
-    console.log(count);
-  } else {
-    console.log('Erorr Code:' + response.statusCode);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
