2390ae3c9fd6e6d97693b8e6fbd22c337edc8d38
Normich ITOUA <nitoua.dev@gmail.com>
2024-07-31 00:25:49 +0100

    Add task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+// Get the API URL from command line arguments
+const apiUrl = process.argv[2];
+
+// Make a GET request to the API
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Invalid status code:', response.statusCode);
+  } else {
+    try {
+      const films = JSON.parse(body).results;
+      const wedgeFilms = films.filter(film =>
+        film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+      );
+      console.log(wedgeFilms.length);
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
+    }
+  }
+});
